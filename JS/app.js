c107ff21364b6ec4ed74783dbe3621ea2f2a8504
César Mantilla <90099252+nmantillaqCertus@users.noreply.github.com>
2023-06-28 02:12:13 -0500

    update 10

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-    inicarMensaje();
+    IniciarReconocimientoVoz();
 });
 
 var artyom = new Artyom();
@@ -16,20 +16,22 @@ function IniciarReconocimientoVoz(){
         speed:1 // Habla normalmente
     }).then(function(){        
         console.log("Reconocimiento de voz iniciardo");
-        artyom.say("Hola estimado cliente, Soy tu SmartOrder virtual. Bienvenido a la cevichería Sonia");
-        artyom.say("Puedes ver el menú con el botón, 'realiza tu pedido'");
+        /*artyom.say("Hola estimado cliente, Soy tu SmartOrder virtual. Bienvenido a la cevichería Sonia");
+        artyom.say("Puedes ver el menú con el botón, 'realiza tu pedido'");*/
+
+        hablar2();
     });
+    
 }
 
 
 function hablar(){
     artyom.say("por favor espere, estoy preparando un menú especial para usted");
-
 }
-function inicarMensaje(){
-    setTimeout(() => {
-        IniciarReconocimientoVoz();
-      }, 1000);
 
+function hablar2(){
+    console.log("Reconocimiento de voz iniciardo");
+    artyom.say("Hola estimado cliente, Soy tu SmartOrder virtual. Bienvenido a la cevichería Sonia");
+    artyom.say("Puedes ver el menú con el botón, 'realiza tu pedido'");
 }
 
