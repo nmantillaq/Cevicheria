ddab08ce7a2e7d6859bc2029c2ab5f3ff6352bc5
César Mantilla <90099252+nmantillaqCertus@users.noreply.github.com>
2023-06-27 23:42:06 -0500

    first commit

---
+++ JS/app.js
@@ -0,0 +1,30 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    startOneCommandArtyom();
+});
+
+var artyom = new Artyom();
+
+
+function startOneCommandArtyom(){
+    artyom.fatality();// Detener cualquier instancia previa
+
+    setTimeout(function(){// Esperar 250ms para inicializar
+         artyom.initialize({
+            lang:"es-ES",// Más lenguajes son soportados, lee la documentación
+            continuous:false,// Reconoce 1 solo comando y basta de escuchar
+            listen:true, // Iniciar !
+            debug:true, // Muestra un informe en la consola
+            speed:1, // Habla normalmente
+            name: "Microsoft Helena - Spanish (Spain)"
+        }).then(function(){
+
+            hablar();
+        });
+    },250);
+}
+
+function hablar(){
+    artyom.say("Hola Bienvenidos");
+
+}
